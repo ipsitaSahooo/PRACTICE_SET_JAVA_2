d9c85fffdc466593a1cc40788a61bf4397327008
ipsitaSahooo <102987358+ipsitaSahooo@users.noreply.github.com>
2023-08-21 20:25:18 +0530

    Create BottomViewOfBt(gfg).java

---
+++ Tree/BottomViewOfBt(gfg).java
@@ -0,0 +1,27 @@
+ ArrayList<Integer> list=new ArrayList<>();
+        if(root == null)
+        {
+            return list;
+        }
+        
+        TreeMap<Integer,Integer> top=new TreeMap<>();
+        
+        Queue<pair> q=new LinkedList<>();
+        q.offer(new pair(root,0));
+        
+        while(!q.isEmpty())
+        {
+            pair p=q.poll();
+            root=p.node;
+            int vertical=p.row;
+            
+            top.putIfAbsent(vertical,root.data);
+            
+            if(root.left!=null)
+            q.offer(new pair(root.left,vertical-1));
+            
+            if(root.right != null)
+            q.offer(new pair(root.right,vertical+1));
+        }
+        list.addAll(top.values());
+        return list;
