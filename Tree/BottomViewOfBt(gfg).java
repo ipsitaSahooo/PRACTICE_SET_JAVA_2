8e32234ed47c69266647bf756df145420e765617
ipsitaSahooo <102987358+ipsitaSahooo@users.noreply.github.com>
2023-08-29 19:25:16 +0530

    Update BottomViewOfBt(gfg).java

--- Tree/BottomViewOfBt(gfg).java
+++ Tree/BottomViewOfBt(gfg).java
@@ -25,3 +25,29 @@
         }
         list.addAll(top.values());
         return list;
+//OR
+class Solution
+{
+   
+    //Function to return a list containing the bottom view of the given tree.
+    public ArrayList <Integer> bottomView(Node root)
+    {
+         ArrayList<Integer> ans=new ArrayList<>();
+        TreeMap<Integer,int[]> hm=new TreeMap<>();
+        help(root,0,0,hm);
+        for(var i:hm.entrySet())
+        {
+            ans.add(i.getValue()[0]);
+        }
+        return ans;
+        
+        
+    }
+    public void help(Node root,int level,int verlevel,TreeMap<Integer,int[]> hm){
+        if(root==null)return;
+        if(!hm.containsKey(verlevel)||hm.get(verlevel)[1]<=level)
+        hm.put(verlevel,new int[]{root.data,level});
+        help(root.left,level+1,verlevel-1,hm);
+        help(root.right,level+1,verlevel+1,hm);
+    }
+}
