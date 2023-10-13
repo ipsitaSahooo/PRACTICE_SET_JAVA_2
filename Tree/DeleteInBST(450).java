312e86000e4ec1cc8a403d308095910887bdc2e3
ipsitaSahooo <102987358+ipsitaSahooo@users.noreply.github.com>
2023-10-13 09:07:24 +0530

    Update DeleteInBST(450).java

--- Tree/DeleteInBST(450).java
+++ Tree/DeleteInBST(450).java
@@ -1,32 +1,40 @@
 class Solution {
     public TreeNode deleteNode(TreeNode root, int key) {
-        if(root==null) return null;
+        if(root==null) 
+        return null;
         
-        if(key<root.val){                            
+        if(key<root.val) //check in left subtree
+        {                            
             root.left = deleteNode(root.left,key);
             return root;
         }
         
-        else if(key>root.val){
+        else if(key>root.val) //check in the right subtree
+        {
             root.right = deleteNode(root.right,key);
             return root;
         }
         
-        else{
-            if(root.left==null){
+        else //root is the node then there will be 3 cases
+        {
+            if(root.left==null) //1st-left root is null,delete root & return right
+            {
                 return root.right;
             }
-            else if(root.right==null){
+            else if(root.right==null)//2nd-right root is null,delete root & return left
+            {
                 return root.left;
             }
-            else{
-                TreeNode min = root.right;
-                while(min.left!=null){
+            else //neither left nor right root is null,delete root but maintain BST
+            {
+                TreeNode min = root.right; //so that root is grtr than all nodes in left and smaller than all nodes in right
+                while(min.left!=null) //get the leftmost node of the right subtree
+                {
                     min = min.left;
                 }
                 
-                root.val = min.val;
-                root.right = deleteNode(root.right,min.val);
+                root.val = min.val;//deleteing root value and replace it by min val
+                root.right = deleteNode(root.right,min.val); //so now just pass that right subtree in which the value is present with that nodes value which will be now the target
                 return root;
             }
         }
