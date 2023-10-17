eb57caf082bdb4ee5606a2e7b699b8b112ad13c3
ipsitaSahooo <102987358+ipsitaSahooo@users.noreply.github.com>
2023-10-17 09:25:01 +0530

    Update lowestCommonAncestorInBT(gfg).java

--- Tree/lowestCommonAncestorInBT(gfg).java
+++ Tree/lowestCommonAncestorInBT(gfg).java
@@ -3,7 +3,8 @@ class Solution
     //Function to return the lowest common ancestor in a Binary Tree.
 	Node lca(Node root, int n1,int n2)
 	{
-	    if(root == null || root.data == n1 || root.data == n2) return root;
+	    if(root == null || root.data == n1 || root.data == n2) 
+		    return root;
 		
 		Node left = lca(root.left, n1, n2);
 		Node right = lca(root.right, n1, n2);
